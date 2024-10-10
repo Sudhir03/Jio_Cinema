5f50cc5d565b74d10f49d4d7d88404a024435b22
amansharma-7 <amansharma40732@gmail.com>
2024-10-10 18:15:53 +0530

    adding suggestions

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import Header from "./Header";
 import Main from "./Main";
+import SuggestedTopics from "./SuggestedTopics";
 
 function App() {
   return (
     <>
       <Header />
+      <SuggestedTopics></SuggestedTopics>
       <Main />
     </>
   );
