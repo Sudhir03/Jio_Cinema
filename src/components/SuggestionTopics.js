11f9b41869a1166eb11d86ebe84c73f889a7d875
amansharma-7 <amansharma40732@gmail.com>
2024-10-10 19:09:00 +0530

    home suggestion done

--- src/components/SuggestionTopics.js
+++ src/components/SuggestionTopics.js
@@ -4,35 +4,35 @@ function SuggestedTopics() {
     return (
         <>
         <div class="  w-full h-[80px] flex justify-between items-center bg-[#0d1a27] px-10  ">           
-            <div className=" pt-1 px-[8px] h-[35px] w-[75px] text-white text-[16px] text-nowrap bg-gray-500 rounded-3xl font-bold cursor-pointer bg-opacity-50 hover:bg-opacity-100" >For You</div> 
+            <div className=" pt-1 px-[8px] h-[35px] w-auto max-w-[150px] text-white text-[16px] text-nowrap bg-gray-500 rounded-3xl font-bold cursor-pointer bg-opacity-50 hover:bg-opacity-100" >For You</div> 
             
-            <div className=" pt-1 px-[8px] h-[35px] w-[75px] text-white text-[16px] text-nowrap bg-gray-500 rounded-3xl font-bold cursor-pointer bg-opacity-50 hover:bg-opacity-100" >INDvsBAN</div> 
+            <div className=" pt-1 px-[8px] h-[35px] w-auto max-w-[150px] text-white text-[16px] text-nowrap bg-gray-500 rounded-3xl font-bold cursor-pointer bg-opacity-50 hover:bg-opacity-100" >INDvsBAN</div> 
             
-            <div className=" pt-1 px-[8px] h-[35px] w-[75px] text-white text-[16px] text-nowrap bg-gray-500 rounded-3xl font-bold cursor-pointer bg-opacity-50 hover:bg-opacity-100" >For You</div> 
+            <div className=" pt-1 px-[8px] h-[35px] w-auto max-w-[150px] text-white text-[16px] text-nowrap bg-gray-500 rounded-3xl font-bold cursor-pointer bg-opacity-50 hover:bg-opacity-100" >Bigg Boss</div> 
             
-            <div className=" pt-1 px-[8px] h-[35px] w-[75px] text-white text-[16px] text-nowrap bg-gray-500 rounded-3xl font-bold cursor-pointer bg-opacity-50 hover:bg-opacity-100" >For You</div> 
+            <div className=" pt-1 px-[8px] h-[35px] w-auto max-w-[150px] text-white text-[16px] text-nowrap bg-gray-500 rounded-3xl font-bold cursor-pointer bg-opacity-50 hover:bg-opacity-100" >BBK</div> 
             
-            <div className=" pt-1 px-[8px] h-[35px] w-[75px] text-white text-[16px] text-nowrap bg-gray-500 rounded-3xl font-bold cursor-pointer bg-opacity-50 hover:bg-opacity-100" >For You</div> 
+            <div className=" pt-1 px-[8px] h-[35px] w-auto max-w-[150px] text-white text-[16px] text-nowrap bg-gray-500 rounded-3xl font-bold cursor-pointer bg-opacity-50 hover:bg-opacity-100" >ISL</div> 
             
-            <div className=" pt-1 px-[8px] h-[35px] w-[75px] text-white text-[16px] text-nowrap bg-gray-500 rounded-3xl font-bold cursor-pointer bg-opacity-50 hover:bg-opacity-100" >For You</div> 
+            <div className=" pt-1 px-[8px] h-[35px] w-auto max-w-[150px] text-white text-[16px] text-nowrap bg-gray-500 rounded-3xl font-bold cursor-pointer bg-opacity-50 hover:bg-opacity-100" >FREE Anime</div> 
             
-            <div className=" pt-1 px-[8px] h-[35px] w-[75px] text-white text-[16px] text-nowrap bg-gray-500 rounded-3xl font-bold cursor-pointer bg-opacity-50 hover:bg-opacity-100" >For You</div> 
+            <div className=" pt-1 px-[8px] h-[35px] w-auto max-w-[150px] text-white text-[16px] text-nowrap bg-gray-500 rounded-3xl font-bold cursor-pointer bg-opacity-50 hover:bg-opacity-100" >FREE Movies</div> 
             
-            <div className=" pt-1 px-[8px] h-[35px] w-[75px] text-white text-[16px] text-nowrap bg-gray-500 rounded-3xl font-bold cursor-pointer bg-opacity-50 hover:bg-opacity-100" >For You</div> 
+            <div className=" pt-1 px-[8px] h-[35px] w-auto max-w-[150px] text-white text-[16px] text-nowrap bg-gray-500 rounded-3xl font-bold cursor-pointer bg-opacity-50 hover:bg-opacity-100" >Cricket</div> 
             
-            <div className=" pt-1 px-[8px] h-[35px] w-[75px] text-white text-[16px] text-nowrap bg-gray-500 rounded-3xl font-bold cursor-pointer bg-opacity-50 hover:bg-opacity-100" >For You</div> 
+            <div className=" pt-1 px-[8px] h-[35px] w-auto max-w-[150px] text-white text-[16px] text-nowrap bg-gray-500 rounded-3xl font-bold cursor-pointer bg-opacity-50 hover:bg-opacity-100" >Kids & Family</div> 
             
-            <div className=" pt-1 px-[8px] h-[35px] w-[75px] text-white text-[16px] text-nowrap bg-gray-500 rounded-3xl font-bold cursor-pointer bg-opacity-50 hover:bg-opacity-100" >For You</div> 
+            <div className=" pt-1 px-[8px] h-[35px] w-auto max-w-[150px] text-white text-[16px] text-nowrap bg-gray-500 rounded-3xl font-bold cursor-pointer bg-opacity-50 hover:bg-opacity-100" >Big Boss Marathi</div> 
             
-            <div className=" pt-1 px-[8px] h-[35px] w-[75px] text-white text-[16px] text-nowrap bg-gray-500 rounded-3xl font-bold cursor-pointer bg-opacity-50 hover:bg-opacity-100" >For You</div> 
+            <div className=" pt-1 px-[8px] h-[35px] w-auto max-w-[150px] text-white text-[16px] text-nowrap bg-gray-500 rounded-3xl font-bold cursor-pointer bg-opacity-50 hover:bg-opacity-100" >Bandminton</div> 
             
-            <div className=" pt-1 px-[8px] h-[35px] w-[75px] text-white text-[16px] text-nowrap bg-gray-500 rounded-3xl font-bold cursor-pointer bg-opacity-50 hover:bg-opacity-100" >For You</div> 
+            <div className=" pt-1 px-[8px] h-[35px] w-auto max-w-[150px] text-white text-[16px] text-nowrap bg-gray-500 rounded-3xl font-bold cursor-pointer bg-opacity-50 hover:bg-opacity-100" >KKK</div> 
             
-            <div className=" pt-1 px-[8px] h-[35px] w-[75px] text-white text-[16px] text-nowrap bg-gray-500 rounded-3xl font-bold cursor-pointer bg-opacity-50 hover:bg-opacity-100" >For You</div> 
+            <div className=" pt-1 px-[8px] h-[35px] w-auto max-w-[150px] text-white text-[16px] text-nowrap bg-gray-500 rounded-3xl font-bold cursor-pointer bg-opacity-50 hover:bg-opacity-100" >Reality</div> 
             
-            <div className=" pt-1 px-[8px] h-[35px] w-[75px] text-white text-[16px] text-nowrap bg-gray-500 rounded-3xl font-bold cursor-pointer bg-opacity-50 hover:bg-opacity-100" >For You</div> 
+            <div className=" pt-1 px-[8px] h-[35px] w-auto max-w-[150px] text-white text-[16px] text-nowrap bg-gray-500 rounded-3xl font-bold cursor-pointer bg-opacity-50 hover:bg-opacity-100" >FREE Shows</div> 
             
-            <div className=" pt-1 px-[8px] h-[35px] w-[75px] text-white text-[16px] text-nowrap bg-gray-500 rounded-3xl font-bold cursor-pointer bg-opacity-50 hover:bg-opacity-100" >For You</div> 
+            <div className=" pt-1 px-[8px] h-[35px] w-auto max-w-[150px] text-white text-[16px] text-nowrap bg-gray-500 rounded-3xl font-bold cursor-pointer bg-opacity-50 hover:bg-opacity-100" >Premium</div> 
 
              
             
